302d7bc4780a89987b607006e5adc97a6daf1078
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-08-09 14:05:38 +0200

    plus help flags

--- phind.go
+++ phind.go
@@ -5,6 +5,8 @@ import (
 	"path/filepath"
 )
 
+var HELP_FLAGS = []string{"h", "-h", "--h", "help", "-help", "--help"}
+
 const HELP string = "\nUsage:\tphind SEARCH [START]\n" +
 	"SEARCH\tFile or directory to search for.\n" +
 	"START\tDirectory where to start searching; " +
@@ -12,7 +14,14 @@ const HELP string = "\nUsage:\tphind SEARCH [START]\n" +
 	"Argument SEARCH can either be a string or match pattern. go docs:\n" +
 	"https://golang.org/pkg/path/filepath/#Match.\n"
 
-var SEARCH, START string
+func contains(slize []string, item string) bool {
+	for _, v := range slize {
+		if v == item {
+			return true
+		}
+	}
+	return false
+}
 
 func exists(path string) bool {
 	_, err := os.Stat(path)
@@ -21,8 +30,8 @@ func exists(path string) bool {
 
 func visitEntry(epath string, fi os.FileInfo, err error) error {
 	if err != nil {
-		os.Stderr.WriteString("error: " + err.Error() + "\n") // can't walk here
-		return nil                                            // but continue
+		os.Stderr.WriteString("error: " + err.Error() + "\n")
+		return nil
 	}
 	matched, err := filepath.Match(SEARCH, fi.Name())
 	if err != nil {
@@ -41,19 +50,25 @@ func visitEntry(epath string, fi os.FileInfo, err error) error {
 	return nil
 }
 
+var SEARCH, START string
+
 func main() {
 	switch len(os.Args) {
 	case 1:
 		os.Stderr.WriteString("error: no arguments\n" + HELP)
 		os.Exit(1)
 	case 2:
+		if contains(HELP_FLAGS, os.Args[1]) {
+			os.Stdout.WriteString(HELP)
+			os.Exit(0)
+		}
 		START, _ = os.Getwd()
 	default:
 		START = os.Args[2]
 	}
 	exs := exists(START)
 	if !exs {
-		os.Stderr.WriteString("error: start directory does not exist\n" + HELP)
+		os.Stderr.WriteString("error: START does not exist\n" + HELP)
 		os.Exit(1)
 	}
 	SEARCH = os.Args[1]
